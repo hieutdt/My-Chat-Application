4a4b9b8c695f2ef4c31a3efc4903c92868a3f9f7
TDTHieu <trandinhtonhieu2@gmail.com>
2019-11-17 14:15:27 +0700

    first commit

---
+++ My Chat/Screens/FriendViewController/FriendViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FriendViewController.swift
+//  My Chat
+//
+//  Created by Trần Đình Tôn Hiếu on 11/17/19.
+//  Copyright © 2019 Trần Đình Tôn Hiếu. All rights reserved.
+//
+
+import Foundation
---
+++ My Chat/Screens/ProfileViewController/ProfileViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileViewController.swift
+//  My Chat
+//
+//  Created by Trần Đình Tôn Hiếu on 11/17/19.
+//  Copyright © 2019 Trần Đình Tôn Hiếu. All rights reserved.
+//
+
+import Foundation
