5cafd94dfb501dfca3c3198e1f46dc414f482828
TDTHieu <trandinhtonhieu2@gmail.com>
2019-11-17 11:07:23 +0700

    Initial Commit

---
+++ My Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Chat
+//
+//  Created by Trần Đình Tôn Hiếu on 11/17/19.
+//  Copyright © 2019 Trần Đình Tôn Hiếu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
